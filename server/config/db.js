e635fecdcfe13e89e96e7dbc20cd43f0d7e7cfb8
laca8 <ahmedragab01140331005@gmail.com>
2024-01-28 16:36:25 +0200

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectedDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://laca:jae09908@cluster0.gjxhg.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://laca:jae09908@cluster0.gjxhg.mongodb.net/stack?retryWrites=true&w=majority"
     );
     console.log("connected on database....");
   } catch (err) {
